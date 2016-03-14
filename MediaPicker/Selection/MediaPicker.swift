e2ce460a050ceea0dfc5de952eaa16bc14a3dda7
Mikhail Stepkin <mikhail.s@ramotion.com>
2016-03-14 16:11:12 +0300

    Video preview fix

--- MediaPicker/Selection/MediaPicker.swift
+++ MediaPicker/Selection/MediaPicker.swift
@@ -264,6 +264,66 @@ public class MediaPicker: UIViewController, ImageSource, VideoSource {
     }
     
     private func setupVideoPreview(asset: AVURLAsset) {
+        async(.Main) { [weak self] in
+            if let `self` = self {
+                let videoPreview = UIView(frame: self.largePreview.bounds)
+                videoPreview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+                videoPreview.backgroundColor = UIColor.purpleColor()
+                self.largePreview.addSubview(videoPreview)
+                
+                self.videoPreview = videoPreview
+                
+                //        videoPreview.remoteURL = asset.URL.absoluteString
+                
+                //        let statusBarHidden = UIApplication.sharedApplication().statusBarHidden
+                let movieController = MPMoviePlayerViewController(contentURL: asset.URL)
+                
+                let player = movieController.view
+                
+                videoPreview.contentMode = .ScaleAspectFill
+                player.contentMode = .ScaleAspectFill
+                
+                player.frame = videoPreview.bounds
+                player.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+                
+                player.removeFromSuperview()
+                videoPreview.addSubview(player)
+                videoPreview.bringSubviewToFront(player)
+                
+                if
+                    let vc = player.viewController as? MPMoviePlayerViewController,
+                    let mp = vc.moviePlayer
+                {
+                    //            mp.controlStyle = .None
+                    mp.shouldAutoplay = false
+                    //            UIApplication.sharedApplication().statusBarHidden = statusBarHidden
+                    //
+                    //            let playControlView = PlayControlView(frame: player.bounds)
+                    //            playControlView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+                    //            player.addSubview(playControlView)
+                    //
+                    //            playControlView.onTouchDown.listen(self) { [weak playControlView] in
+                    //                if let state = playControlView?.playControlState {
+                    //                    switch state {
+                    //                    case .Pause:
+                    //                        mp.pause()
+                    //                    case .Play:
+                    //                        mp.play()
+                    //                    default:
+                    //                        break
+                    //                    }
+                    //                }
+                    //            }
+                    //            playControlView.playControlState = .Play
+                    //            playControlView.enableTouch = true
+                    //            self.videoPlayControl = playControlView
+                    //
+                    //            NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("videoPlaybackStateChanged:"), name: MPMoviePlayerPlaybackStateDidChangeNotification, object: mp)
+                    //            NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("videoWentFullscreen:"), name: MPMoviePlayerWillEnterFullscreenNotification, object: mp)
+                    //            NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("videoReturnedFromFullscreen:"), name: MPMoviePlayerDidExitFullscreenNotification, object: mp)
+                }
+            }
+        }
     }
     
     private var statusBarHidden: Bool = UIApplication.sharedApplication().statusBarHidden
@@ -345,16 +405,16 @@ public class MediaPicker: UIViewController, ImageSource, VideoSource {
                     {
                         let outputFileURL = urlAsset.URL
                         
-//                        let videoVC = VideoCropViewController(videoURL: outputFileURL)
-//                        videoVC.onVideoReady = { [weak self] asset -> Void in
-//                            if let `self` = self {
-//                                self.onVideoReady?(asset)
-//                            }
-//                        }
-//                        videoVC.onClose = { [weak self] in
-//                            self?.navigationController?.popViewControllerAnimated(true)
-//                        }
-//                        self.navigationController?.pushViewController(videoVC, animated: true)
+                        //                        let videoVC = VideoCropViewController(videoURL: outputFileURL)
+                        //                        videoVC.onVideoReady = { [weak self] asset -> Void in
+                        //                            if let `self` = self {
+                        //                                self.onVideoReady?(asset)
+                        //                            }
+                        //                        }
+                        //                        videoVC.onClose = { [weak self] in
+                        //                            self?.navigationController?.popViewControllerAnimated(true)
+                        //                        }
+                        //                        self.navigationController?.pushViewController(videoVC, animated: true)
                     }
                 }
             default:
